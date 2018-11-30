a2b5657cf94efd8600dda175475058cabd590646
Collin Moon <mooncollin@gmail.com>
2018-11-29 21:45:24 -0600

    added activity

---
+++ activities/OneLED/OneLED.ino
@@ -0,0 +1,26 @@
+#include <Potentiometer.h>
+#include <Led.h>
+
+acm::Led l(9, 10, 11);
+acm::Pot redPot(2);
+acm::Pot greenPot(1);
+acm::Pot bluePot(0);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int red = acm::Led::potConversion(redPot);
+  int green = acm::Led::potConversion(greenPot);
+  int blue = acm::Led::potConversion(bluePot);
+  Serial.print("Red: ");
+  Serial.print(red);
+  Serial.print(" Green: ");
+  Serial.print(green);
+  Serial.print(" Blue: ");
+  Serial.println(blue);
+  l.setColor(redPot, greenPot, bluePot);
+}
---
+++ activities/OneLED/README.md
@@ -0,0 +1,8 @@
+# OneLED
+This activity uses three potentiometers to change the red, green, and blue component of an RGB LED.
+
+## This activity requires:
+* LEDs
+* Potentiometers
+
+Note: Schematics for this activity will be added soon.
